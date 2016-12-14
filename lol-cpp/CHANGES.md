3f15861f2651b276dda9e67dc9816693c383b024
Eric <ecrockett0@gmail.com>
2016-12-13 21:00:42 -0500

    Renamed challenges to rlwe-challenges

--- lol-cpp/CHANGES.md
+++
@@ -1,6 +0,0 @@
-Changelog for lol-cpp project
-================================
-
-0.0.0.1
-----
- * Initial split from lol package.
--- lol-cpp/Crypto/Lol/Cyclotomic/Tensor/CPP/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMON_H_
-#define COMMON_H_
-
-#include "types.h"
-
-// calculates base ** exp
-hDim_t ipow(hDim_t base, hShort_t exp);
-
-#endif /* COMMON_H_ */
